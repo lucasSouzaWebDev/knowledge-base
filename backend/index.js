bdf9a902949984cb85f615ceb72c76683a2d9551
Lucas de Souza <lucassd.k13@gmail.com>
2022-09-20 20:46:05 -0300

    Defined middlewares and user route.

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,12 @@
 const app = require('express')()
+const consign = require('consign')
+
+consign()
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
+
 const port = 3000
 
 app.listen(port, () => console.log(`Backend is running on http://localhost:${port}`))

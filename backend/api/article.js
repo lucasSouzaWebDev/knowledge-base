ee7c2477eb83b1528baad6548756e5c40bd0be80
Lucas de Souza <lucassd.k13@gmail.com>
2022-09-22 19:43:04 -0300

    Developing article API.

---
+++ backend/api/article.js
@@ -0,0 +1,77 @@
+module.exports = (app) => {
+  const { existsOrError } = app.api.validation;
+
+  const save = (req, res) => {
+    const article = { ...req.body };
+    if (req.params.id) article.id = req.params.id;
+
+    try {
+      existsOrError(article.name, "Nome não informado");
+      existsOrError(article.description, "Descrição não informada");
+      existsOrError(article.categoryId, "Categoria não informada");
+      existsOrError(article.userId, "Autor não informado");
+      existsOrError(article.content, "Conteúdo não informado");
+    } catch (msg) {
+      res.status(400).send(msg);
+    }
+
+    if (article.id) {
+      app
+        .db("articles")
+        .update(article)
+        .where({ id: article.id })
+        .then((_) => res.status(204).send())
+        .catch((error) => res.status(500).send(error));
+    } else {
+      app
+        .db("articles")
+        .insert(article)
+        .then((_) => res.status(204).send())
+        .catch((error) => res.status(500).send(error));
+    }
+  };
+
+  const remove = async (req, res) => {
+    try {
+      const rowsDeleted = await app
+        .db("articles")
+        .where({ id: req.params.id })
+        .del();
+      existsOrError(rowsDeleted, "Artigo não foi encontrado.");
+
+      res.status(204).send();
+    } catch (msg) {
+      res.status(500).send(msg);
+    }
+  };
+
+  const limit = 10;
+  const get = async (req, res) => {
+    const page = req.query.page || 1;
+
+    const result = await app.db("articles").count("id").first();
+    const count = parseInt(result.count);
+
+    app
+      .db("articles")
+      .select("id", "name", "description")
+      .limit(limit)
+      .offset(page * limit - limit)
+      .then((articles) => res.json({ data: articles, count, limit }))
+      .catch((error) => res.status(500).send(error));
+  };
+
+  const getById = (req, res) => {
+    app
+      .db("articles")
+      .where({ id: req.params.id })
+      .first()
+      .then((article) => {
+        article.content = article.content.toString();
+        return res.json(article);
+      })
+      .catch((error) => res.status(500).send(error));
+  };
+
+  return { save, remove, get, getById };
+};
--- backend/migrations/20220920205857_create_table_articles.js
+++ backend/migrations/20220920205857_create_table_articles.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
         table.increments('id').primary()
         table.string('name').notNull()
         table.string('description', 1000).notNull()
-        table.string('image_url', 1000).notNull()
+        table.string('image_url', 1000)
         table.binary('content').notNull()
         table.integer('userId').references('id').inTable('users').notNull()
         table.integer('categoryId').references('id').inTable('categories').notNull()

5cb391cedcda202a77943bc04d5d2314e17934cf
Lucas de Souza <lucassd.k13@gmail.com>
2022-09-24 16:21:07 -0300

    Updated env_file with basic database connection config.

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,13 +1,8 @@
-// Update with your config settings.
+const { db } = require('./.env')
 
 module.exports = {
   client: "postgresql",
-  connection: {
-    host: "localhost",
-    database: "knowledge",
-    user: "postgres",
-    password: "12345678",
-  },
+  connection: db,
   pool: {
     min: 2,
     max: 10,

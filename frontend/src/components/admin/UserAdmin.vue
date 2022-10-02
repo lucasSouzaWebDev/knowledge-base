8b5544d90dc19cad22f1f360ea5ac21029f6dcbd
Lucas de Souza <lucassd.k13@gmail.com>
2022-10-02 15:14:48 -0300

    Created user admin table

--- frontend/src/components/admin/UserAdmin.vue
+++ frontend/src/components/admin/UserAdmin.vue
@@ -1,14 +1,37 @@
 <template>
   <div class="user-admin">
-    <h1>Usuário componente</h1>
+    <b-table hover striped :items="users" :fields="fields"></b-table>
   </div>
 </template>
   
 <script>
-
+import { baseApiUrl } from '@/global';
+import axios from 'axios'
 
 export default {
-  name: 'UserAdmin'
+  name: 'UserAdmin',
+  data: function () {
+    return {
+      mode: 'save',
+      user: {},
+      users: [],
+      fields: [
+        { key: 'id', label: 'Código', sortable: true },
+        { key: 'name', label: 'Nome', sortable: true },
+        { key: 'email', label: 'E-mail', sortable: true },
+        { key: 'admin', label: 'Administrador', sortable: true, formatter: value => value ? 'Sim' : 'Não' },
+        { key: 'actions', label: 'Ações' }
+      ]
+    }
+  },
+  methods: {
+    loadUsers() {
+      axios.get(`${baseApiUrl}/users`).then(response => { this.users = response.data })
+    }
+  },
+  mounted() {
+    this.loadUsers();
+  }
 }
 </script>
   
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,6 +8,8 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Imx1Y2FzIiwiZW1haWwiOiJsdWNhc0BnbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjY0NTg1NDgwLCJleHAiOjE2NjQ4NDQ2ODB9.I0bGVaQHI3s5NdBRcHHFpzfT-j2qZVJFnhl98w1WsCo'
+
 new Vue({
   store,
   router,

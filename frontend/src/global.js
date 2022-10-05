caf69e1647e9ccb14ae5da1df06452aa8f0e8d85
lucasSouzaWebDev <lucasdesouzadubena@gmail.com>
2022-10-05 20:59:18 -0300

    Created users via form

--- frontend/src/global.js
+++ frontend/src/global.js
@@ -4,9 +4,9 @@ export const baseApiUrl = 'http://localhost:3000'
 
 export function showError(error) {
     if(error && error.response && error.response.data){
-        Vue.toasted.global.defaultError({ msg: e.response.data })
-    } else if(typeof e === 'string'){
-        Vue.toasted.global.defaultError({ msg: e })
+        Vue.toasted.global.defaultError({ msg: error.response.data })
+    } else if(typeof error === 'string'){
+        Vue.toasted.global.defaultError({ msg: error })
     } else{
         Vue.toasted.global.defaultError()
     }
